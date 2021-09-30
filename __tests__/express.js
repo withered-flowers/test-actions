94148b6f1f96f114126c276cd0534829324cb465
Withered Flowers <withered-flowers@users.noreply.github.com>
2021-09-30 14:36:11 +0700

    fix: workflows actions

--- __tests__/express.js
+++ __tests__/express.js
@@ -1,6 +1,18 @@
 const { assert } = require("console");
 const request = require("supertest");
+const { User } = require("../models/index");
 const app = require("../app.js");
+
+beforeAll(() => {
+  User.create({
+    email: "Testing",
+    password: "123123123",
+  });
+});
+
+afterAll(() => {
+  User.destroy({ truncate: true });
+});
 
 describe("GET /", function () {
   it("response with Hello World", function (done) {
@@ -9,6 +21,23 @@ describe("GET /", function () {
       .expect(200)
       .then((response) => {
         assert(response.body.msg, "Hello World");
+        done();
+      });
+  });
+});
+
+describe("GET /user", function () {
+  it("response with Array of object Users", function (done) {
+    request(app)
+      .get("/user")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.data).toEqual(
+          expect.arrayContaining({
+            email: "Testing",
+            password: "123123123",
+          })
+        );
         done();
       });
   });
--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
+const { User } = require("./models/index");
+
 app.get("/", async (req, res) => {
   res.status(200).json({ msg: "Hello World" });
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const response = await User.findAll();
+
+    res.status(200).json({ data: response });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = app;
